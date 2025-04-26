23f1597c28af62e8e3b95b7cab472ea1997c01ee
Mayank Vishwakarma <aryan8319@gmail.com>
2025-04-26 13:25:36 +0530

    python add lowest

--- findinglowestnumber.py
+++ findinglowestnumber.py
@@ -2,9 +2,8 @@ arr = [3,0,4,1,5,9,-1,6,5,3,5]
 # findlowest number in the array
 def findLowest(arr):
     lowest = arr[0]
-    for i in arr:
-        if i < lowest:
-            lowest = i
+    for i in range(len(arr)):        
+     if arr[i] < lowest:
+      lowest = arr[i]
     return lowest
-print(findLowest(arr)) # 1
-# find the lowest number in the array using recursion           
+print(findLowest(arr))
